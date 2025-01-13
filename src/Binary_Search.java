876bdf2229ec0790ac2af1073066d032caa02bc4
Chirayu Mitra <155830642+Chirayu1411Mitra@users.noreply.github.com>
2025-01-13 23:41:34 +0530

    Update Binary_Search.java

--- src/Binary_Search.java
+++ src/Binary_Search.java
@@ -19,7 +19,7 @@ public class Binary_Search {
     //this is for Accending array
 
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] arr = {1, 2, 3, 4, 5, 7, 8, 9};
         Scanner input = new Scanner(System.in);
         int target = input.nextInt();
         int index = BS(arr, target);
