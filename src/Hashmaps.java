ab30263f4930701df7ad94daa0fde56b2f4cce05
Chirayu1411Mitra <mitrachirayu@gmail.com>
2025-03-08 19:30:51 +0530

    .

---
+++ src/Hashmaps.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Scanner;
+public class Hashmaps{
+	public static void main(String[] args){
+	// create a hashmap
+	Hashmap<Strings,Integer>map=new Hashmap<>()
+	//add element in hahsmap
+map.put("Appple", 20);
+map.put("Banana", 30);
+// print out using key value 
+System.out.println(map.get("Apple"));
+// to check if key value exists 
+System.outprintln(mao.contains.key("Apple"));
+//to remove the element form hashmap
+map.remove("Apple");
+
+
+}
+
+
+}
